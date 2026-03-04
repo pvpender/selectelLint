954108a05a94b3eb341d034b3d69185f17e54a3e
pvpender <nicolaymoiseev1@gmail.com>
2026-03-05 01:45:07 +0300

    feat: start work

--- cmd/selectelLint/main.go
+++ cmd/selectelLint/main.go
@@ -1,1 +1,10 @@
-package selectelLint
+package main
+
+import (
+	"golang.org/x/tools/go/analysis/singlechecker"
+	"pvpender/selectelLint/analyzers/selectelLint"
+)
+
+func main() {
+	singlechecker.Main(selectelLint.NewAnalyzer())
+}
--- config/config.go
+++ config/config.go
@@ -1,1 +1,24 @@
 package config
+
+type Rule struct {
+}
+
+type Config struct {
+	CapitalLetter     bool
+	EnglishLetter     bool
+	SpecialLetters    bool
+	SensitiveData     bool
+	EnableCustomRules bool
+	Rules             []Rule
+}
+
+func NewConfig() *Config {
+	return &Config{
+		CapitalLetter:     true,
+		EnglishLetter:     true,
+		SpecialLetters:    true,
+		SensitiveData:     true,
+		EnableCustomRules: false,
+		Rules:             nil,
+	}
+}
