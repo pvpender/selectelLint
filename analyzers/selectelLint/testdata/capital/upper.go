7712a309b51e5e9a982ccab4ff6e1a9f0c137074
pvpender <nicolaymoiseev1@gmail.com>
2026-03-06 15:02:38 +0300

    feat: new tests

--- analyzers/selectelLint/testdata/capital/upper.go
+++ analyzers/selectelLint/testdata/capital/upper.go
@@ -3,7 +3,8 @@ package capital
 import "log/slog"
 
 func TestSimple() {
-	slog.Info("Test") // want `Start uppercase detected!`
+	slog.Info("Test")       // want `Start uppercase detected!`
+	slog.Debug("I am here") // want `Start uppercase detected!`
 }
 
 func Normal() {
